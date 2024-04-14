afe3ddde50db3ea18f9828881a06e625465f9029
Ismaël MOHAMED BOUH <ismaelmedbouh@outlook.fr>
2024-04-14 11:56:53 +0200

    Setup database tables.

--- app/(main)/agency/page.tsx
+++ app/(main)/agency/page.tsx
@@ -1,4 +1,11 @@
-const AgencyPage = () => {
+import { currentUser } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
+
+const AgencyPage = async () => {
+  const authUser = await currentUser();
+  if (!authUser) {
+    return redirect('/sign-in');
+  }
   return <div>AgencyPage</div>;
 };
 
